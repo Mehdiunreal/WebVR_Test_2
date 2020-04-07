55d97a2e648071619dc7642eed4d501a1249ecbd
AndreAroCiVR <45397854+AndreAroCiVR@users.noreply.github.com>
2020-04-07 15:07:02 +0200

    Initial commit

---
+++ Assets/InstantiateBalls.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBalls : MonoBehaviour
+{
+    public GameObject targetBall;
+
+    private void Start()
+    {
+        InvokeRepeating("InstantiateTheBall", 10, 5);
+    }
+    void Update()
+    {
+    }
+
+    void InstantiateTheBall()
+    {
+        Instantiate(targetBall, this.transform.position, Quaternion.identity);
+    }
+}
